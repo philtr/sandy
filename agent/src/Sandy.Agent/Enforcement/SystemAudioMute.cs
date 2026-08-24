33cece5184d2d401cef3dfe5866af9eccf0593ed
Phillip Ridlen <p@rdln.net>
2026-08-24 01:15:59 -0500

    fix(agent): allow Core Audio interface activation

--- agent/src/Sandy.Agent/Enforcement/SystemAudioMute.cs
+++ agent/src/Sandy.Agent/Enforcement/SystemAudioMute.cs
@@ -112,7 +112,7 @@ internal sealed class SystemAudioMute : IDisposable
 
     [ComImport]
     [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
-    private sealed class MMDeviceEnumerator
+    private class MMDeviceEnumerator
     {
     }
 
